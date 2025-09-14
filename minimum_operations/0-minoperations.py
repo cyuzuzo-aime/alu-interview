ecfae64beaad44cdcfe6be55c1dd9b0e4249301b
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-09-14 23:53:23 +0200

    fix: pep8 validation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,8 +4,8 @@ Calculate the minimum number of Copy All and Paste operations
 to get exactly n 'H' characters.
 """
 
-def minOperations(n):
 
+def minOperations(n):
     """
     Return the minimum operations to reach n 'H's.
 
@@ -13,17 +13,17 @@ def minOperations(n):
 
     Returns: int (minimum operations)
     """
-  
     # if n is not a number or less than 2 Hs
     if not isinstance(n, int) or n < 2:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
+
     return operations
